f8da2af3875e1d77a3b8bcbca7f031f984735b18
HashamArif <55347196+HashamArif@users.noreply.github.com>
2026-04-13 06:33:00 +0500

    Delete android_device_transsion_x662b directory

--- android_device_transsion_x662b/Android.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),x662b_h6913)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
--- android_device_transsion_x662b/AndroidProducts.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_x662b_h6913.mk
-COMMON_LUNCH_CHOICES := omni_x662b_h6913-eng omni_x662b_h6913-userdebug
